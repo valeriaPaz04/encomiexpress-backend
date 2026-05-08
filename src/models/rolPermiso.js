3879b952955edbafc77a333aa62b92fec4e73099
Valeria <valeriapaz010203@outlook.com>
2026-05-08 15:36:03 -0500

    fix: remove idRolPermiso from RolPermiso model to fix permission loading

--- src/models/rolPermiso.js
+++ src/models/rolPermiso.js
@@ -2,12 +2,6 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const RolPermiso = sequelize.define('RolPermiso', {
-  idRolPermiso: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-    field: 'idRolPermiso'
-  },
   idRol: {
     type: DataTypes.INTEGER,
     allowNull: false
